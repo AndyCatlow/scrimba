d1e3404bc09a172555c27400312adb2130a78d78
AndyCatlow <mrcatlow@gmail.com>
2021-06-17 21:06:16 +0200

    add palindromechecker solution

---
+++ jschallenges/palindromechecker.js
@@ -0,0 +1,48 @@
+function caseInsensitivePalindrome(str) {
+  //  str to lowercase
+  str = str.toLowerCase();
+  console.log(str);
+  // check first letter = last letter
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+caseInsensitivePalindrome("aaabbb");
+
+/**
+ * Test Suite
+ */
+describe("caseInsensitivePalindrome()", () => {
+  it("returns true for a case insensitive palindrome", () => {
+    // arrange
+    const str = "AaBaa";
+
+    // act
+    const result = caseInsensitivePalindrome(str);
+
+    // log
+    console.log("result 1: ", result);
+
+    // assert
+    expect(result).toBe(true);
+  });
+
+  it("returns false when not a case insensitive palindrome", () => {
+    // arrange
+    const str = "abac";
+
+    // act
+    const result = caseInsensitivePalindrome(str);
+
+    // log
+    console.log("result 2: ", result);
+
+    // assert
+    expect(result).toBe(false);
+  });
+});
